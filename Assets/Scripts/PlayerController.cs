b8f1be14fd153cac580988d01ddfce9805092f5c
Ben Bennett <bbennett@uoregon.edu>
2018-01-10 13:19:26 -0800

    added skybox

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float topSpeed;
+    public float acceleration;
+    public float idleDeceleration;
+    public float breakingDeceleration;
+
+    private float currentSpeed = 0.0f;
+    private Vector3 speedVector = new Vector3();
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            currentSpeed = Mathf.Max(0.0f, currentSpeed - breakingDeceleration * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            currentSpeed = Mathf.Min(topSpeed, currentSpeed + acceleration * Time.deltaTime);
+        }
+        else
+        {
+            currentSpeed = Mathf.Max(0.0f, currentSpeed - idleDeceleration * Time.deltaTime);
+        }
+        
+	}
+
+    private void FixedUpdate()
+    {
+        speedVector.z = currentSpeed * Time.fixedDeltaTime;
+        this.gameObject.transform.position += speedVector;
+    }
+}
---
+++ Assets/Scripts/StoplightController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoplightController : MonoBehaviour {
+    public float greenTime;
+    public float yellowTime;
+    public float redTime;
+
+    public Material redMat;
+    public Material greenMat;
+    public Material yellowMat;
+    public Material offMat;
+
+    public GameObject greenLight;
+    public GameObject yellowLight;
+    public GameObject redLight;
+
+    private float _elapsed = 0f;
+    private int _currentState = 0;
+
+	// Use this for initialization
+	void Start () {
+        greenLight.GetComponent<Renderer>().material = greenMat;
+        yellowLight.GetComponent<Renderer>().material = offMat;
+        redLight.GetComponent<Renderer>().material = offMat;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        _elapsed += Time.deltaTime;
+
+        if(_currentState == 0 && _elapsed >= greenTime)
+        {
+            // transition to yellow
+            greenLight.GetComponent<Renderer>().material = offMat;
+            yellowLight.GetComponent<Renderer>().material = yellowMat;
+            _currentState = 1;
+            _elapsed = 0f;
+        }
+        else if(_currentState == 1 && _elapsed >= yellowTime)
+        {
+            // transition to red
+            yellowLight.GetComponent<Renderer>().material = offMat;
+            redLight.GetComponent<Renderer>().material = redMat;
+            _currentState = 2;
+            _elapsed = 0f;
+        }
+        else if(_currentState == 2 && _elapsed >= redTime)
+        {
+            // transition to green
+            greenLight.GetComponent<Renderer>().material = greenMat;
+            redLight.GetComponent<Renderer>().material = offMat;
+            _currentState = 0;
+            _elapsed = 0f;
+        }
+	}
+}
